d216ebf5026200817c442f3c30f420f4c4c09499
hugomflavio <hmflavio92@gmail.com>
2025-04-06 17:37:48 -0400

    update filtering functions to use dot workers

--- R/filter_functions.R
+++ R/filter_functions.R
@@ -1,25 +1,3 @@
-#' wrapper to filter detections by ping deviation bands
-#' 
-#' @inheritParams filter_det
-#' @param bands a vector of deviation bands to filter
-#' @param grace width of the bands to extract
-#' 
-#' @return the updated ATO
-#' 
-filter_ping_dev <- function(object, bands, grace, hard = FALSE) {
-  band_list <- lapply(bands, function(i) c(i - grace, i + grace))
-  if (!has(object, "det")) {
-    message("M: No detections to filter.")
-  }
-  for (i in 1:length(band_list)) {
-    message("M: Filtering band ", bands[i])
-    if (has(object, "det")) {
-      object <- filter_det(object, ping_dev = band_list[[i]], hard = hard)
-    }
-  }
-  return(object)
-}
-
 #' Filter rows from the @det slot
 #' 
 #' @param object an \code{\link[ATO]{ATO}}
@@ -40,63 +18,15 @@ filter_ping_dev <- function(object, bands, grace, hard = FALSE) {
 filter_det <- function(object, ..., hard = FALSE) {
   is_ato(object)
   has(object, "det", error = TRUE)
-  
+
   checks <- list(...)
   if (length(checks) == 0) {
     stop("No filtering arguments provided.", call. = FALSE)
   }
 
-  # for each requested variable, check which rows would be kept
-  keep_these <- lapply(names(checks), function(i) {
-    if (!(i %in% colnames(object@det))) {
-      warning("Variable ", i, " not found in @det. Skipping.",
-              immediate. = TRUE, call. = FALSE)
-      output <- NULL
-    } else {
-      if (length(checks[[i]]) == 2 & is.numeric(checks[[i]])) {
-        output <- object@det[, i, with = FALSE] >= checks[[i]][1] &
-                  object@det[, i, with = FALSE] <= checks[[i]][2]
-      } else {
-        output <- object@det[, i, with = FALSE] %in% checks[[i]]
-      }
-    }
-    return(output)
-  })
-  keep_these <- keep_these[!sapply(keep_these, is.null)]
-
-  if (length(keep_these) == 0) {
-    stop("None of the requested variables exist in @det.",
-         call. = FALSE)
-  } 
+  filter_vec <- .create_filter_vec(object, checks, "det")
+  object <- .apply_filter(object, filter_vec, "det", hard = hard)
   
-  # only the rows that would be kept by all variables
-  # should be kept.
-  keep_these <- data.table::as.data.table(keep_these)
-  to_keep_check <- NULL # to avoid rcmdcheck note
-  keep_these[, to_keep_check := all(.SD), by = .I]
-  keep_these <- keep_these$to_keep_check
-
-  if (all(keep_these)) {
-    message("M: All detections match the filtering criteria.")
-  } else {
-    aux1 <- sum(keep_these)
-    aux2 <- nrow(object@det)
-    if (hard) {
-      new_dets <- object@det[keep_these, ]
-      message("M: Subset ", aux1,
-              " detections (",
-              .dyn_round(aux1 / aux2 * 100),
-              "%) that match the filtering criteria.")
-      # using add is an easy way to trigger the match checks again
-      object <- add(object, new_dets)
-    } else {
-      object@det$valid[!keep_these] <- FALSE
-      message("M: ", aux1,
-              " detections (",
-              .dyn_round(aux1 / aux2 * 100),
-              "%) remain valid after applying the filtering criteria.")
-    }
-  }
   return(object)
 }
 
@@ -122,48 +52,9 @@ filter_dep <- function(object, ...) {
     stop("No filtering arguments provided.", call. = FALSE)
   }
 
-  # for each requested variable, check which rows would be kept
-  keep_these <- lapply(names(checks), function(i) {
-    if (!(i %in% colnames(object@dep))) {
-      warning("Variable ", i, " not found in @dep. Skipping.",
-              immediate. = TRUE, call. = FALSE)
-      output <- NULL
-    } else {
-      if (length(checks[[i]]) == 2 & is.numeric(checks[[i]])) {
-        output <- object@dep[, i, with = FALSE] >= checks[[i]][1] &
-                  object@dep[, i, with = FALSE] <= checks[[i]][2]
-      } else {
-        output <- object@dep[, i, with = FALSE] %in% checks[[i]]
-      }
-    }
-    return(output)
-  })
-  keep_these <- keep_these[!sapply(keep_these, is.null)]
+  filter_vec <- .create_filter_vec(object, checks, "dep")
+  object <- .apply_filter(object, filter_vec, "dep", hard = TRUE)
 
-  if (length(keep_these) == 0) {
-    stop("None of the requested variables exist in @dep.",
-         call. = FALSE)
-  } 
-  
-  # only the rows that would be kept by all variables
-  # should be ketp
-  keep_these <- data.table::as.data.table(keep_these)
-  to_keep_check <- NULL # to avoid rcmdcheck note
-  keep_these[, to_keep_check := all(.SD), by = .I]
-  keep_these <- keep_these$to_keep_check
-
-  if (all(keep_these)) {
-    message("M: All deployments match the filtering criteria.")
-  } else {
-    aux <- sum(keep_these)
-    message("M: Filtered ", aux,
-            ifelse(aux > 1,
-                   " deployments.",
-                   " deployment."))
-    new_deps <- object@dep[keep_these, ]
-    # using add is an easy way to trigger the match checks again
-    object <- add(object, new_deps) 
-  }
   return(object)
 }
 
@@ -187,68 +78,49 @@ filter_dep <- function(object, ...) {
 filter_obs <- function(object, ..., hard = FALSE) {
   is_ato(object)
   has(object, "obs", error = TRUE)
-
+  
   checks <- list(...)
   if (length(checks) == 0) {
     stop("No filtering arguments provided.", call. = FALSE)
   }
 
-  # for each requested variable, check which rows would be kept
-  keep_these <- lapply(names(checks), function(i) {
-    if (!(i %in% colnames(object@obs))) {
-      warning("Variable ", i, " not found in @obs. Skipping.",
-              immediate. = TRUE, call. = FALSE)
-      output <- NULL
-    } else {
-      if (length(checks[[i]]) == 2 & is.numeric(checks[[i]])) {
-        output <- object@obs[, i, with = FALSE] >= checks[[i]][1] &
-                  object@obs[, i, with = FALSE] <= checks[[i]][2]
-      } else {
-        output <- object@obs[, i, with = FALSE] %in% checks[[i]]
-      }
-    }
-    return(output)
-  })
-  keep_these <- keep_these[!sapply(keep_these, is.null)]
+  filter_vec <- .create_filter_vec(object, checks, "obs")
+  object <- .apply_filter(object, filter_vec, "obs", hard)
 
-  if (length(keep_these) == 0) {
-    stop("None of the requested variables exist in @obs.",
-         call. = FALSE)
-  } 
-  
-  # only the rows that would be kept by all variables
-  # should be kept.
-  keep_these <- data.table::as.data.table(keep_these)
-  to_keep_check <- NULL # to avoid rcmdcheck note
-  keep_these[, to_keep_check := all(.SD), by = .I]
-  keep_these <- keep_these$to_keep_check
+  rm(obs)
+  return(object)
+}
 
-  if (all(keep_these)) {
-    message("M: All observations match the filtering criteria.")
+#' wrapper to filter detections by ping deviation bands
+#' 
+#' @inheritParams filter_det
+#' @param bands a vector of deviation bands to filter
+#' @param grace width of the bands to extract
+#' 
+#' @return the updated ATO
+#' 
+filter_ping_dev <- function(object, bands, grace, hard = FALSE) {
+  band_list <- lapply(bands, function(i) c(i - grace, i + grace))
+  if (!has(object, "det")) {
+    message("M: No detections to filter.")
   } else {
-    aux1 <- sum(keep_these)
-    aux2 <- nrow(object@obs)
-    if (hard) {
-      new_obs <- object@obs[keep_these, ]
-      message("M: Subset ", aux1,
-              " observations (",
-              .dyn_round(aux1 / aux2 * 100),
-              "%) that match the filtering criteria.")
-      # using add is an easy way to trigger the match checks again
-      object <- add(object, new_obs)
-    } else {
-      object@obs$valid[!keep_these] <- FALSE
-      message("M: ", aux1,
-              " observations (",
-              .dyn_round(aux1 / aux2 * 100),
-              "%) remain valid after applying the filtering criteria.")
-    }
+    recipient <- lapply(1:length(band_list), function(i) {
+      suppressMessages(filter_det(object, ping_dev = band_list[[i]],
+                 hard = FALSE)@det$valid)
+    })
+    checker <- data.table::as.data.table(recipient)
+    to_run <- paste0("checker[, valid := ",
+                     paste0(paste0("V", 1:ncol(checker)), collapse = " | "),
+                     "]")
+    eval(parse(text = to_run))
+
+    object <- .apply_filter(object, checker$valid, "det", hard = hard)
   }
   return(object)
 }
 
 #' Wrapper to filter detections and observations that match the
-#' transmitters in the tag slot.
+#' transmitters in the tag and dep slots.
 #' 
 #' @inheritParams filter_det
 #' 
@@ -257,17 +129,19 @@ filter_obs <- function(object, ..., hard = FALSE) {
 #' @export
 #' 
 filter_targeted <- function(object, hard = FALSE) {
-  if (!has(object, "tag")) {
-    stop("The ATO's tag slot is empty.", call. = FALSE)
+  if (!has(object, "tag") & !has(object, "dep")) {
+    stop("Both the tag and the dep slotes are empty. Can't filter.", call. = FALSE)
   }
+  targets <- c(object@tag$transmitter, object@dep$transmitter)
+
   if (has(object, "det")) {
-    object <- filter_det(object, transmitter = object@tag$transmitter)
+    object <- filter_det(object, transmitter = targets, hard = hard)
   } else {
     message("M: No detections to filter.")
   }
 
   if (has(object, "obs")) {
-    object <- filter_obs(object, transmitter = object@tag$transmitter)
+    object <- filter_obs(object, transmitter = targets, hard = hard)
   } else {
     message("M: No observations to filter.")
   }
@@ -294,7 +168,7 @@ filter_datetime <- function(object, from = "1970-01-01", to = "3000-01-01",
     if (is(to, "character")) {
       to <- as.POSIXct(to, tz = tz)
     }
-    object <- filter_det(object, datetime = c(from, to))
+    object <- filter_det(object, datetime = c(from, to), hard = hard)
   } else {
     message("M: No detections to filter.")
   }
@@ -307,9 +181,29 @@ filter_datetime <- function(object, from = "1970-01-01", to = "3000-01-01",
     if (is(to, "character")) {
       to <- as.POSIXct(to, tz = tz)
     }
-    object <- filter_obs(object, datetime = c(from, to))
+    object <- filter_obs(object, datetime = c(from, to), hard = hard)
   } else {
     message("M: No observations to filter.")
   }
+  return(object)
+}
+
+#' Clear all previously applied (non-hard) filters
+#' 
+#' @param object an \code{\link[ATO]{ATO}}
+#' 
+#' @return the updated ATO
+#' 
+#' @export
+#' 
+filter_reset <- function(object) {
+  is_ato(object)
+  has(object, "det", error = TRUE)
+
+  # use data.table for fast processing
+  object@det$valid <- TRUE
+  
+  message("M: All ", nrow(object@det), " detections switched back to valid.")
+  
   return(object)
 }
