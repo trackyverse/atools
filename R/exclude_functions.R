e945d2245b68e1da93c088dd048b66765bcf8974
hugomflavio <hmflavio92@gmail.com>
2026-03-25 16:24:09 -0400

    pushing everything so Mike can have a look

--- R/exclude_functions.R
+++ R/exclude_functions.R
@@ -1,290 +1,43 @@
-#' Exclude rows from the @ani slot
+#' Exclude rows from an ATO slot
 #'
 #' If exclusion parameters are not provided, removes the entire slot.
 #'
 #' @param object an \code{\link[ATO]{ATO}}
 #' @param ... named arguments to exclude by. The argument's name must match
-#'   one of the column names in the animals slot. If only one value is provided,
+#'   one of the column names in the target slot. If only one value is provided,
 #'   data is excluded by exact match. If two values are provided for continuous
 #'   variables, data is filtered within the interval provided. If three or more
 #'   values are provided, data is filtered by exact match. If no arguments
 #'   are provided, the entire slot is removed.
-#' @param hard If false (the default), animals are flagged as invalid through
-#'   the invalid column, but kept in the dataset. Switch to true to completely
-#'   remove the animals from the dataset (useful to improve performance when
-#'   handling very large datasets). Ignored if no exclusion arguments
-#'   are provided.
+#' @param slt The slot to be filtered.
+#' @param hard If false (the default), animal rows are flagged as invalid
+#'   through the valid column, but kept in the dataset. Switch to true to drop
+#'   the rows that do not fit the filtering criteria (useful to improve 
+#'   performance when handling very large datasets).
+#' @param silent Logical: Supress summary messages
 #'
 #' @return the updated ATO
 #'
 #' @export
 #'
-exclude_ani <- function(object, ..., hard = FALSE) {
+exclude <- function(object, slt = c("ani", "dep", "det", "tag", "obs"),
+                    ..., hard = FALSE, silent = FALSE) {
+  slt <- match.arg(slt)
   is_ato(object)
-  has(object, "ani", error = TRUE)
+  has(object, slt, error = TRUE)
 
   checks <- list(...)
   if (length(checks) == 0) {
-    object@ani <- ATO::.ATO_ani
-    if (has(object, "det")) {
-      object@det$animal <- NULL
-    }
-    if (has(object, "tag")) {
-      object@tag$ani_match <- NULL
-    }
-    if (has(object, "obs")) {
-      object@obs$animal <- NULL
-    }
-    return(object)
+    to_run <- paste0("object@", slt, " <- ATO::.ATO_", slt)
+    eval(parse(text = to_run))
+  } else {
+    filter_vec <- create_filter_vec(object, slt, checks)
+    object <- .apply_filter(object, filter_vec, exclude = TRUE,
+                            slt = slt, hard = hard)
   }
 
-  filter_vec <- .create_filter_vec(object, checks, "ani")
-  object <- .apply_filter(
-    object,
-    filter_vec,
-    exclude = TRUE,
-    slt = "ani",
-    hard = hard
-  )
-  if (has(object, "det")) {
-    object <- match_det_ani(object, silent = TRUE)
-  }
-  if (has(object, "tag")) {
-    object <- match_tag_ani(object, silent = TRUE)
-  }
-  if (has(object, "obs")) {
-    object <- match_obs_ani(object, silent = TRUE)
-  }
-  return(object)
-}
-
-#' Exclude rows from the @dep slot
-#'
-#' If exclusion parameters are not provided, removes the entire slot.
-#'
-#' @param object an \code{\link[ATO]{ATO}}
-#' @param ... named arguments to filter for. The argument's name must match
-#'   one of the column names in the deployments. If only one value is provided,
-#'   data is filtered by exact match. If two values are provided for continuous
-#'   variables, data is filtered within the interval provided. If three or more
-#'   values are provided, data is filtered by exact match.
-#'
-#' @return the updated ATO
-#'
-#' @export
-#'
-exclude_dep <- function(object, ...) {
-  is_ato(object)
-  has(object, "dep", error = TRUE)
-
-  checks <- list(...)
-  if (length(checks) == 0) {
-    object@dep <- ATO::.ATO_dep
-    if (has(object, "det")) {
-      # reset tag match. There's probably a 
-      # more efficient way of doing this
-      object@det$tag_match <- NULL
-      if (has(object, "tag")) {
-        object <- match_det_tag(object, silent = TRUE)
-      }
-    }
-    return(object)
-  }
-
-  filter_vec <- .create_filter_vec(object, checks, "dep")
-  object <- .apply_filter(
-    object,
-    filter_vec,
-    exclude = TRUE,
-    slt = "dep",
-    hard = TRUE
-  )
-
-  if (has(object, "det")) {
-    # reset tag match. There's probably a 
-    # more efficient way of doing this
-    object@det$tag_match <- NULL
-    object <- match_det_dep(object, silent = TRUE)
-    if (has(object, "tag")) {
-      object <- match_det_tag(object, silent = TRUE)
-    }
-  }
-  return(object)
-}
-
-#' Exclude rows from the @det slot
-#'
-#' If exclusion parameters are not provided, removes the entire slot.
-#'
-#' @param object an \code{\link[ATO]{ATO}}
-#' @param ... named arguments to filter for. The argument's name must match
-#'   one of the column names in the detections. If only one value is provided,
-#'   data is excluded by exact match. If two values are provided for continuous
-#'   variables, data is filtered within the interval provided. If three or more
-#'   values are provided, data is filtered by exact match. If no arguments
-#'   are provided, the entire slot is removed.
-#' @param hard If false (the default), detections are flagged as invalid through
-#'   the invalid column, but kept in the dataset. Switch to true to completely
-#'   remove the detections from the dataset (useful to improve performance when
-#'   handling very large datasets). Ignored if no exclusion arguments
-#'   are provided.
-#'
-#' @return the updated ATO
-#'
-#' @export
-#'
-exclude_det <- function(object, ..., hard = FALSE) {
-  is_ato(object)
-  has(object, "det", error = TRUE)
-
-  checks <- list(...)
-  if (length(checks) == 0) {
-    object@det <- ATO::.ATO_det
-    if (has(object, "ani")) {
-      object@tag$det_match <- NULL
-    }
-    if (has(object, "dep")) {
-      object@dep$n_detections <- NULL
-    }
-    if (has(object, "tag")) {
-      object@tag$det_match <- NULL
-    }
-    return(object)
-  }
-
-  filter_vec <- .create_filter_vec(object, checks, "det")
-  object <- .apply_filter(
-    object,
-    filter_vec,
-    exclude = TRUE,
-    slt = "det",
-    hard = hard
-  )
-
-  if (has(object, "ani")) {
-    object <- match_det_ani(object, silent = TRUE)
-  }
-  if (has(object, "tag")) {
-    object <- match_det_tag(object, silent = TRUE)
-  }
-  if (has(object, "dep")) {
-    object <- match_det_dep(object, silent = TRUE)
-  }
-  return(object)
-}
-
-#' Exclude rows from the @obs slot
-#'
-#' If exclusion parameters are not provided, removes the entire slot.
-#'
-#' @param object an \code{\link[ATO]{ATO}}
-#' @param ... named arguments to filter for. The argument's name must match
-#'   one of the column names in the observations. If only one value is provided,
-#'   data is excluded by exact match. If two values are provided for continuous
-#'   variables, data is filtered within the interval provided. If three or more
-#'   values are provided, data is filtered by exact match. If no arguments
-#'   are provided, the entire slot is removed.
-#' @param hard If false (the default), observations are flagged as invalid through
-#'   the invalid column, but kept in the dataset. Switch to true to completely
-#'   remove the detections from the dataset (useful to improve performance when
-#'   handling very large datasets). Ignored if no exclusion arguments
-#'   are provided.
-#'
-#' @return the updated ATO
-#'
-#' @export
-#'
-exclude_obs <- function(object, ..., hard = FALSE) {
-  is_ato(object)
-  has(object, "obs", error = TRUE)
-
-  checks <- list(...)
-  if (length(checks) == 0) {
-    object@obs <- ATO::.ATO_obs
-    if (has(object, "ani")) {
-      object@tag$obs_match <- NULL
-    }
-    if (has(object, "tag")) {
-      object@tag$obs_match <- NULL
-    }
-    return(object)
-  }
-
-  filter_vec <- .create_filter_vec(object, checks, "obs")
-  object <- .apply_filter(
-    object,
-    filter_vec,
-    exclude = TRUE,
-    slt = "obs",
-    hard = hard
-  )
-
-  if (has(object, "tag")) {
-    object <- match_obs_tag(object, silent = TRUE)
-  }
-  if (has(object, "ani")) {
-    object <- match_obs_ani(object, silent = TRUE)
-  }
-  return(object)
-}
-
-#' Exclude rows from the @tag slot
-#'
-#' If exclusion parameters are not provided, removes the entire slot.
-#'
-#' @param object an \code{\link[ATO]{ATO}}
-#' @param ... named arguments to filter for. The argument's name must match
-#'   one of the column names in the tags slot If only one value is provided,
-#'   data is excluded by exact match. If two values are provided for continuous
-#'   variables, data is filtered within the interval provided. If three or more
-#'   values are provided, data is filtered by exact match. If no arguments
-#'   are provided, the entire slot is removed.
-#' @param hard If false (the default), tags are flagged as invalid through
-#'   the invalid column, but kept in the dataset. Switch to true to completely
-#'   remove the detections from the dataset (useful to improve performance when
-#'   handling very large datasets). Ignored if no exclusion arguments
-#'   are provided.
-#'
-#' @return the updated ATO
-#'
-#' @export
-#'
-exclude_tag <- function(object, ..., hard = FALSE) {
-  is_ato(object)
-  has(object, "tag", error = TRUE)
-
-  checks <- list(...)
-  if (length(checks) == 0) {
-    object@tag <- ATO::.ATO_tag
-    if (has(object, "tag")) {
-      object@det$tag_match <- NULL
-    }
-    if (has(object, "obs")) {
-      object@dep$tag_match <- NULL
-    }
-    if (has(object, "ani")) {
-      object@ani$tag_match <- NULL
-    }
-    return(object)
-  }
-
-  filter_vec <- .create_filter_vec(object, checks, "tag")
-  object <- .apply_filter(
-    object,
-    filter_vec,
-    exclude = TRUE,
-    slt = "tag",
-    hard = hard
-  )
-
-  if (has(object, "det")) {
-    object <- match_det_tag(object, silent = TRUE)
-  }
-  if (has(object, "obs")) {
-    object <- match_obs_tag(object, silent = TRUE)
-  }
-  if (has(object, "ani")) {
-    object <- match_tag_ani(object, silent = TRUE)
+  if (getOption("ATO_match_immediate", default = TRUE)) {
+    object <- match_update(silent = TRUE)
   }
   return(object)
 }
@@ -305,8 +58,8 @@ exclude_orphan_dets <- function(object, hard = FALSE) {
   is_ato(object)
   has(object, "det", error = TRUE)
 
-  object <- exclude_det(object, dep_match = NA,
-                        hard = hard)
+  object <- exclude(object, "det", dep_match = NA,
+                    hard = hard)
 
   return(object)
 }
--- R/filter_functions.R
+++ R/filter_functions.R
@@ -1,4 +1,4 @@
-#' Filter rows from the @ani slot
+#' Filter rows from an ATO slot
 #' 
 #' @param object an \code{\link[ATO]{ATO}}
 #' @param ... named arguments to filter for. The argument's name must match
@@ -6,204 +6,195 @@
 #'   data is filtered by exact match. If two values are provided for continuous
 #'   variables, data is filtered within the interval provided. If three or more
 #'   values are provided, data is filtered by exact match.
+#' @param slt The slot to be filtered.
 #' @param hard If false (the default), animal rows are flagged as invalid
 #'   through the valid column, but kept in the dataset. Switch to true to drop
 #'   the tags that do not fit the filtering criteria (useful to improve 
 #'   performance when handling very large datasets).
+#' @param silent Logical: Supress summary messages
 #' 
 #' @return the updated ATO
 #' 
 #' @export
 #' 
-filter_ani <- function(object, ..., hard = FALSE) {
+filter <- function(object, slt = c("ani", "dep", "det", "tag", "obs"),
+                   ..., hard = FALSE, silent = FALSE) {
+  slt <- match.arg(slt)
   is_ato(object)
-  has(object, "ani", error = TRUE)
+  has(object, slt, error = TRUE)
   
   checks <- list(...)
   if (length(checks) == 0) {
     stop("No filtering arguments provided.", call. = FALSE)
   }
 
-  filter_vec <- .create_filter_vec(object, checks, "ani")
+  filter_vec <- create_filter_vec(object, slt, checks)
   object <- .apply_filter(object, filter_vec, exclude = FALSE,
-                          slt = "ani", hard)
+                          slt = slt, hard = hard)
 
-  if (has(object, "det")) {
-    object <- match_det_ani(object, silent = TRUE)
-  }
-  if (has(object, "obs")) {
-    object <- match_obs_ani(object, silent = TRUE)
-  }
-  if (has(object, "tag")) {
-    object <- match_tag_ani(object, silent = TRUE)
+  if (getOption("ATO_match_immediate", default = TRUE)) {
+    object <- match_update(silent = TRUE)
   }
   return(object)
 }
 
-#' Filter rows from the @dep slot
+#' Worker function to create filtering vector
 #' 
-#' @param object an \code{\link[ATO]{ATO}}
-#' @param ... named arguments to filter for. The argument's name must match
-#'   one of the column names in the deployments If only one value is provided,
-#'   data is filtered by exact match. If two values are provided for continuous
-#'   variables, data is filtered within the interval provided. If three or more
-#'   values are provided, data is filtered by exact match.
+#' @param object an ATO
+#' @param slt the ATO slot to filter
+#' @param checks a list of column names and respective values to filter with
 #' 
-#' @return the updated ATO
+#' @return a logical vector
 #' 
 #' @export
 #' 
-filter_dep <- function(object, ...) {
-  is_ato(object)
-  has(object, "dep", error = TRUE)
+create_filter_vec <- function(object, slt = c("ani", "dep", "det", "tag", "obs"),
+                              checks) {
+  longslot <- switch(slt,
+                     ani = "animals",
+                     dep = "deployments",
+                     det = "detections",
+                     obs = "observations",
+                     tag = "tags")
 
-  checks <- list(...)
-  if (length(checks) == 0) {
-    stop("No filtering arguments provided.", call. = FALSE)
+  # use data.table for fast processing
+  if (table_type(object) == "data.table") {
+    checker <- slot(object, slt)
+  } else {
+    checker <- data.table::as.data.table(slot(object, slt))
   }
 
-  filter_vec <- .create_filter_vec(object, checks, "dep")
-  object <- .apply_filter(object, filter_vec, exclude = FALSE,
-                          slt = "dep", hard = TRUE)
-
-  if (has(object, "det")) {
-    # reset tag match. There's probably a 
-    # more efficient way of doing this
-    object@det$tag_match <- NULL
-    object <- match_det_dep(object, silent = TRUE)
-    if (has(object, "tag")) {
-      object <- match_det_tag(object, silent = TRUE)
+  # for each requested variable, check which rows would be kept
+  eval_cols <- c()
+  for (i in names(checks)) {
+    if (!(i %in% colnames(slot(object, slt)))) {
+      warning("Variable ", i, " not found in @", slt, ". Skipping.",
+              immediate. = TRUE, call. = FALSE)
+    } else {
+      eval_cols <- c(eval_cols, paste0("filter_check_", i))
+      if (length(checks[[i]]) == 1 && is.na(checks[[i]])) {
+        to_run <- paste0("checker[, filter_check_", i,
+                         " := is.na(", i, ")]")
+      } else {
+        if (length(checks[[i]]) == 2 &
+            (is.double(checks[[i]]) | is.integer(checks[[i]]))) {
+          to_run <- paste0("checker[, filter_check_", i,
+                           " := ",
+                           i, " >= checks[[i]][1] & ",
+                           i, " <= checks[[i]][2]]")
+        } else {
+          to_run <- paste0("checker[, filter_check_", i,
+                           " := ",
+                           i, " %in% checks[[i]]]")
+        }
+      }
+      eval(parse(text = to_run))
     }
   }
-  return(object)
+
+  if (length(eval_cols) == 0) {
+    stop("None of the requested variables exist in @", slt, ".",
+         call. = FALSE)
+  } 
+  
+  to_run <- paste0("checker[, filter_check_pass := ",
+                   paste0(eval_cols, collapse = " & "),
+                   "]")
+  eval(parse(text = to_run))
+
+  output <- checker$filter_check_pass
+
+  # clean up if using data.table
+  if (table_type(object) == "data.table") {
+    for (i in names(checks)) {
+      if ((i %in% colnames(slot(object, slt)))) {
+        to_run <- paste0("checker[, filter_check_", i,
+                         " := NULL]")
+      }
+      eval(parse(text = to_run))
+    }
+    checker$filter_check_pass <- NULL
+  }
+  
+  return(output)
 }
 
-#' Filter rows from the @det slot
+#' Worker function to apply filtering
 #' 
-#' @param object an \code{\link[ATO]{ATO}}
-#' @param ... named arguments to filter for. The argument's name must match
-#'   one of the column names in the detections. If only one value is provided,
-#'   data is filtered by exact match. If two values are provided for continuous
-#'   variables, data is filtered within the interval provided. If three or more
-#'   values are provided, data is filtered by exact match.
-#' @param hard If false (the default), detections are flagged as invalid through
-#'   the invalid column, but kept in the dataset. Switch to true to drop
-#'   detections that do not fit the filtering criteria (useful to improve 
-#'   performance when handling very large datasets).
+#' @param object an ATO
+#' @param filter_vec a logical vector, normally created by
+#'   \code{\link{.create_filter_vec}}.
+#' @param exclude FALSE to filter by filter_vec, TRUE to exclude by filter_vec. 
+#' @param slt the ATO slot to filter
+#' @param hard Should the filtering mark rows as invalid (FALSE) or remove them
+#'   altogether (TRUE)
 #' 
 #' @return the updated ATO
 #' 
-#' @export
+#' @keywords internal
 #' 
-filter_det <- function(object, ..., hard = FALSE) {
-  is_ato(object)
-  has(object, "det", error = TRUE)
-
-  checks <- list(...)
-  if (length(checks) == 0) {
-    stop("No filtering arguments provided.", call. = FALSE)
-  }
-
-  filter_vec <- .create_filter_vec(object, checks, "det")
-  object <- .apply_filter(object, filter_vec, exclude = FALSE,
-                          slt = "det", hard = hard)
-  
-  if (has(object, "ani")) {
-    object <- match_det_ani(object, silent = TRUE)
-  }
-  if (has(object, "tag")) {
-    object <- match_det_tag(object, silent = TRUE)
-  }
-  if (has(object, "dep")) {
-    object <- match_det_dep(object, silent = TRUE)
-  }
-  return(object)
-}
-
-#' Filter rows from the @obs slot
-#' 
-#' @param object an \code{\link[ATO]{ATO}}
-#' @param ... named arguments to filter for. The argument's name must match
-#'   one of the column names in the observations. If only one value is provided,
-#'   data is filtered by exact match. If two values are provided for continuous
-#'   variables, data is filtered within the interval provided. If three or more
-#'   values are provided, data is filtered by exact match.
-#' @param hard If false (the default), observations are flagged as invalid
-#'   through the valid column, but kept in the dataset. Switch to true to drop
-#'   observations that do not fit the filtering criteria (useful to improve 
-#'   performance when handling very large datasets).
-#' 
-#' @return the updated ATO
-#' 
-#' @export
-#' 
-filter_obs <- function(object, ..., hard = FALSE) {
-  is_ato(object)
-  has(object, "obs", error = TRUE)
-  
-  checks <- list(...)
-  if (length(checks) == 0) {
-    stop("No filtering arguments provided.", call. = FALSE)
+.apply_filter <- function(object, filter_vec, exclude,
+                          slt = c("ani", "dep", "det", "tag", "obs"),
+                          hard = FALSE) {
+  longslot <- switch(slt,
+                     ani = "animals",
+                     dep = "deployments",
+                     det = "detections",
+                     obs = "observations",
+                     tag = "tags")
+  if (exclude) {
+    # inverting the vector makes it so we only need one
+    # pipeline for the filtering. All the variation then
+    # is in the messaging.
+    filter_vec <- !filter_vec
   }
 
-  filter_vec <- .create_filter_vec(object, checks, "obs")
-  object <- .apply_filter(object, filter_vec, exclude = FALSE,
-                          slt = "obs", hard)
+  if (all(filter_vec)) {
+    if (exclude) {
+      message("M: No ", longslot, " match the exclusion criteria.")
+    } else {
+      message("M: All ", longslot, " match the filtering criteria.")
+    }
+  } else {
+    old_total <- nrow(slot(object, slt))
+    if (hard) {
+      filtered <- slot(object, slt)[filter_vec, ]
+      new_total <- sum(abs(exclude - filter_vec))
+      message(ifelse(exclude,
+                     "M: Removed ", 
+                     "M: Subset "), 
+              new_total,
+              " ", longslot, " (",
+              .dyn_round(new_total / old_total * 100),
+              "%).")
+      # using add is an easy way to trigger the match checks again
+      object <- add(object, filtered, silent = TRUE)
+      rm(filtered)
+    } else {
+      if (exclude) {
+        new_total <- sum(slot(object, slt)$valid & !filter_vec)
+      } else {
+        new_total <- sum(slot(object, slt)$valid & filter_vec)
+      }
+      slot(object, slt)$valid[!filter_vec] <- FALSE
 
-  if (has(object, "tag")) {
-    object <- match_obs_tag(object, silent = TRUE)
-  }
-  if (has(object, "ani")) {
-    object <- match_obs_ani(object, silent = TRUE)
-  }
-  return(object)
-}
-
-#' Filter rows from the @tag slot
-#' 
-#' @param object an \code{\link[ATO]{ATO}}
-#' @param ... named arguments to filter for. The argument's name must match
-#'   one of the column names in the tags slot. If only one value is provided,
-#'   data is filtered by exact match. If two values are provided for continuous
-#'   variables, data is filtered within the interval provided. If three or more
-#'   values are provided, data is filtered by exact match.
-#' @param hard If false (the default), tag rows are flagged as invalid
-#'   through the valid column, but kept in the dataset. Switch to true to drop
-#'   the tags that do not fit the filtering criteria (useful to improve 
-#'   performance when handling very large datasets).
-#' 
-#' @return the updated ATO
-#' 
-#' @export
-#' 
-filter_tag <- function(object, ..., hard = FALSE) {
-  is_ato(object)
-  has(object, "tag", error = TRUE)
-  
-  checks <- list(...)
-  if (length(checks) == 0) {
-    stop("No filtering arguments provided.", call. = FALSE)
-  }
-
-  filter_vec <- .create_filter_vec(object, checks, "tag")
-  object <- .apply_filter(object, filter_vec, exclude = FALSE,
-                          slt = "tag", hard)
-
-  if (has(object, "det")) {
-    object <- match_det_tag(object, silent = TRUE)
-  }
-  if (has(object, "obs")) {
-    object <- match_obs_tag(object, silent = TRUE)
-  }
-  if (has(object, "ani")) {
-    object <- match_tag_ani(object, silent = TRUE)
+      message(ifelse(exclude,
+                     "M: Rendered ", 
+                     "M: "), 
+              new_total,
+              " ", longslot, " (",
+              .dyn_round(new_total / old_total * 100),
+              ifelse(exclude,
+                     "%) invalid.",
+                     "%) remain valid."))
+    }
   }
   return(object)
 }
 
 #' wrapper to filter detections by ping deviation bands
 #' 
-#' @inheritParams filter_det
+#' @inheritParams filter
 #' @param bands a vector of deviation bands to filter
 #' @param grace width of the bands to extract
 #' 
@@ -212,12 +203,16 @@ filter_tag <- function(object, ..., hard = FALSE) {
 #' @export
 #'
 filter_ping_dev <- function(object, bands, grace, hard = FALSE) {
+  old_match_immediate <- getOption("ATO_match_immediate", default = TRUE)
+  on.exit(options(ATO_match_immediate = old_match_immediate))
+
   band_list <- lapply(bands, function(i) c(i - grace, i + grace))
   if (!has(object, "det")) {
     message("M: No detections to filter.")
   } else {
+    options(ATO_mach_immediate = FALSE)
     recipient <- lapply(1:length(band_list), function(i) {
-      suppressMessages(filter_det(object, ping_dev = band_list[[i]],
+      suppressMessages(filter(object, "det", ping_dev = band_list[[i]],
                  hard = FALSE)@det$valid)
     })
     checker <- data.table::as.data.table(recipient)
@@ -229,6 +224,11 @@ filter_ping_dev <- function(object, bands, grace, hard = FALSE) {
     object <- .apply_filter(object, checker$valid, exclude = FALSE,
                           slt = "det", hard = hard)
   }
+
+  if (old_match_immediate) {
+    object <- match_update(object)
+  }
+
   if (any(object@det$time_diff[object@det$valid] < 0.5)) {
     message("Note: Valid detections with very small ping intervals (>500ms)",
             " found. Could these be reflections?")
@@ -239,7 +239,7 @@ filter_ping_dev <- function(object, bands, grace, hard = FALSE) {
 #' Wrapper to filter detections and observations that match the
 #' transmitters in the tag and dep slots.
 #' 
-#' @inheritParams filter_det
+#' @inheritParams filter
 #' 
 #' @return the updated ATO
 #' 
@@ -252,13 +252,13 @@ filter_targeted <- function(object, hard = FALSE) {
   targets <- c(object@tag$transmitter, object@dep$transmitter)
 
   if (has(object, "det")) {
-    object <- filter_det(object, transmitter = targets, hard = hard)
+    object <- filter(object, "det", transmitter = targets, hard = hard)
   } else {
     message("M: No detections to filter.")
   }
 
   if (has(object, "obs")) {
-    object <- filter_obs(object, transmitter = targets, hard = hard)
+    object <- filter(object, "obs", transmitter = targets, hard = hard)
   } else {
     message("M: No observations to filter.")
   }
@@ -267,7 +267,7 @@ filter_targeted <- function(object, hard = FALSE) {
 
 #' Wrapper to filter detections and observations within the desired time period
 #' 
-#' @inheritParams filter_det
+#' @inheritParams filter
 #' @param from Filter data from this timestamp (included). Can be posixct or character.
 #' @param to Filter data up to this timestamp (included). Can be posixct or character.
 #' 
@@ -285,7 +285,7 @@ filter_datetime <- function(object, from = "1970-01-01", to = "3000-01-01",
     if (is(to, "character")) {
       to <- as.POSIXct(to, tz = tz)
     }
-    object <- filter_det(object, datetime = c(from, to), hard = hard)
+    object <- filter(object, "det", datetime = c(from, to), hard = hard)
   } else {
     message("M: No detections to filter.")
   }
@@ -298,7 +298,7 @@ filter_datetime <- function(object, from = "1970-01-01", to = "3000-01-01",
     if (is(to, "character")) {
       to <- as.POSIXct(to, tz = tz)
     }
-    object <- filter_obs(object, datetime = c(from, to), hard = hard)
+    object <- filter(object, "obs", datetime = c(from, to), hard = hard)
   } else {
     message("M: No observations to filter.")
   }
@@ -308,20 +308,21 @@ filter_datetime <- function(object, from = "1970-01-01", to = "3000-01-01",
 #' Clear all previously applied (non-hard) filters
 #' 
 #' @param object an \code{\link[ATO]{ATO}}
+#' @param s the slot to be reset. If not provided, all slots are reset.
 #' 
 #' @return the updated ATO
 #' 
 #' @export
 #' 
-filter_reset <- function(object) {
+filter_reset <- function(object, s) {
   is_ato(object)
-  has(object, "det", error = TRUE)
-
-  if (all(object@det$valid)) {
-    message("M: All ", nrow(object@det), " detections are already valid.")
+  if (missing(s)) {
+    s <- c("det", "dep", "tag", "ani", "obs=")
   }
-  # use data.table for fast processing
-  object@det$valid <- TRUE
-
+  for (i in s) {
+    if (has(object, i)) {
+      slot(object, i)$valid <- TRUE
+    }
+  }
   return(object)
 }
