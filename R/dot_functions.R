73918e0ec15b83cc2eefa18e832e7acc022d63a3
Hugo Flávio <40389021+hugomflavio@users.noreply.github.com>
2026-05-01 18:47:39 -0300

    copy .comma function from ATO

--- R/dot_functions.R
+++ R/dot_functions.R
@@ -14,3 +14,65 @@
   }
   return(round(value, dec))
 }
+
+#' Concatenate vectors with commas
+#' 
+#' @param x The vector to write out
+#' @param max_i maximum number of vector elements to display before
+#'  saying "and n others"
+#' 
+#' @return a string
+#' 
+#' @keywords internal
+#' 
+#' tests:
+#'   .comma(LETTERS[1])
+#'   .comma(LETTERS[1:2])
+#'   .comma(LETTERS[1:3])
+#'   .comma(LETTERS[1:5])
+#'   .comma(LETTERS[1:10])
+#'
+#'   .comma(LETTERS[1], max_i = 2)
+#'   .comma(LETTERS[1:2], max_i = 2)
+#'   .comma(LETTERS[1:3], max_i = 2)
+#'   .comma(LETTERS[1:5], max_i = 2)
+#'   .comma(LETTERS[1:10], max_i = 2)
+#'
+#'   .comma(LETTERS[1], max_i = 1)
+#'   .comma(LETTERS[1:2], max_i = 1)
+#'   .comma(LETTERS[1:3], max_i = 1)
+#'   .comma(LETTERS[1:5], max_i = 1)
+#'   .comma(LETTERS[1:10], max_i = 1)
+#'
+.comma <- function(x, max_i = 5) {
+  if (max_i < 2) {
+    if (length(x) == 1) {
+      output <- x
+    } else {
+      output <- paste0(x[1], " and ", length(x)-1,
+                      ifelse(length(x)-1 == 1,
+                             " other",
+                             " others"))
+    }
+  } else {
+    if (length(x) == 1) {
+      output <- x
+    }
+    if (length(x) == 2) {
+      output <- paste0(x[1], " and ", x[2])  
+    }
+    if (length(x) > 2) {
+      if (length(x) <= max_i) {
+        output <- paste0(paste0(x[-length(x)], collapse = ", "),
+                         ", and ", x[length(x)])
+      } else {
+        output <- paste0(paste0(x[1:max_i], collapse = ", "),
+                         " and ", length(x)-max_i,
+                         ifelse(length(x)-max_i == 1,
+                                " other",
+                                " others"))
+      }
+    }
+  }
+  return(output)
+}
